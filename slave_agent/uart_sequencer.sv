9b0e524c5876a162b35381e6b68d64106354b609
prakhar53m <prakharmall1999@gmail.com>
2026-05-11 12:19:46 +0530

    Delete slave_agent directory

--- slave_agent/uart_sequencer.sv
+++
@@ -1,7 +0,0 @@
-class uart_sequencer extends uvm_sequencer #(read_xtn);
-`uvm_component_utils(uart_sequencer)
-
-function new(string name="uart_sequencer",uvm_component parent);
-super.new(name,parent);
-endfunction
-endclass
